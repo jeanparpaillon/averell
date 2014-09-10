6f48d947190b8e0971f3f831b347696ddd0ca5e5
Jean Parpaillon <jean.parpaillon@free.fr>
2014-09-10 13:00:03 +0200

    Start needed applications

--- src/averell.erl
+++ src/averell.erl
@@ -1,10 +1,10 @@
 -module(averell).
 
--define(info(Msg), error_logger:info_msg(Msg)).
--define(info(Msg, Data), error_logger:info_msg(Msg, Data)).
+-define(info(Msg), io:format("INFO: " ++ Msg)).
+-define(info(Msg, Data), io:format("INFO: " ++ Msg, Data)).
 
--define(error(Msg), error_logger:error_msg(Msg)).
--define(error(Msg, Data), error_logger:error_msg(Msg, Data)).
+-define(error(Msg), io:format("ERROR: " ++ Msg)).
+-define(error(Msg, Data), io:format("ERROR: " ++ Msg, Data)).
 
 -define(port, 8000).
 
@@ -23,7 +23,7 @@ main([Dir]) ->
 	true ->
 	    start(Dir, ?port);
 	false ->
-	    ?error("Not a dir: ~p~n", [Dir]),
+	    ?error("Not a dir: ~s~n", [Dir]),
 	    halt(1)
     end;
 main(_) ->
@@ -37,8 +37,26 @@ usage() ->
     io:format("Usage: averell [dir]~n").
 
 start(Dir, Port) ->
+    application:start(ranch),
+    application:start(crypto),
+    application:start(cowlib),
+    application:start(cowboy),
     Handler = {"/[...]", cowboy_static, 
 	       {dir, Dir, [{mimetypes, cow_mimetypes, all}]}},
     Dispatch = cowboy_router:compile([{'_', [Handler]}]),
-    cowboy:start_http(http, 5, [{port, Port}], 
-		      [{env, [{dispatch, Dispatch}]}]).
+    case cowboy:start_http(http, 5, [{port, Port}], 
+			   [{env, [{dispatch, Dispatch}]}]) of
+	{ok, _} ->
+	    ?info("Serving ~p on 0.0.0.0:~p~n", [Dir, Port]),
+	    loop();
+	{error, Err} ->
+	    ?error("Error starting server: ~p~n", [Err]),
+	    halt(1)
+    end.
+
+loop() ->
+    receive
+	_ ->
+	    loop()
+    end.
+
