2f04654b1d074d40d4842a8e5b080f57f6d5ccad
Jean Parpaillon <jean.parpaillon@free.fr>
2014-09-10 13:55:23 +0200

    Use getopt and add verbose option

--- src/averell.erl
+++ src/averell.erl
@@ -1,3 +1,26 @@
+%%%-------------------------------------------------------------------
+%%% @author Jean Parpaillon <jean.parpaillon@free.fr>
+%%% @copyright (C) 2014, Jean Parpaillon
+%%% 
+%%% This file is provided to you under the Apache License,
+%%% Version 2.0 (the "License"); you may not use this file
+%%% except in compliance with the License.  You may obtain
+%%% a copy of the License at
+%%% 
+%%%   http://www.apache.org/licenses/LICENSE-2.0
+%%% 
+%%% Unless required by applicable law or agreed to in writing,
+%%% software distributed under the License is distributed on an
+%%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%%% KIND, either express or implied.  See the License for the
+%%% specific language governing permissions and limitations
+%%% under the License.
+%%% 
+%%% @doc
+%%%
+%%% @end
+%%% Created : 10 Sep 2014 by Jean Parpaillon <jean.parpaillon@free.fr>
+%%%-------------------------------------------------------------------
 -module(averell).
 
 -define(info(Msg), io:format("INFO: " ++ Msg)).
@@ -8,46 +31,63 @@
 
 -define(port, 8000).
 
--export([main/1]).
+-define(argspec, [
+		  {help,    $h,        "help",    undefined,           "Show help"},
+		  {port,    $p,        "port",    {integer, ?port},    "Port number"},
+		  {verbose, $v,        undefined, undefined,           "Verbose"},
+		  {dir,     undefined, undefined, {string, get_cwd()}, "Directory to serve"}
+		 ]).
 
-main([]) ->
-    case file:get_cwd() of
-	{ok, Dir} ->
-	    start(Dir, ?port);
-	{error, Reason} ->
-	    ?error("Unable to get cwd: ~p~n", [Reason]),
+-export([main/1, 
+	 onresponse/4]).
+
+main(Args) ->
+    case getopt:parse(?argspec, Args) of
+	{ok, {Opts, []}} ->
+	    case proplists:get_bool(help, Opts) of
+		true ->
+		    getopt:usage(?argspec, atom_to_list(?MODULE)),
+		    halt(0);
+		false ->
+		    start(Opts)
+	    end;
+	_ ->
+	    getopt:usage(?argspec, atom_to_list(?MODULE)),
 	    halt(1)
-    end;
-main([Dir]) ->
-    case filelib:is_dir(Dir) of
-	true ->
-	    start(Dir, ?port);
-	false ->
-	    ?error("Not a dir: ~s~n", [Dir]),
-	    halt(1)
-    end;
-main(_) ->
-    usage(),
-    halt(1).
+    end.
 
 %%%
 %%% Priv
 %%%
-usage() ->
-    io:format("Usage: averell [dir]~n").
+get_cwd() ->
+    case file:get_cwd() of
+	{ok, Dir} ->
+	    Dir;
+	{error, Reason} ->
+	    ?error("Unable to get cwd: ~p~n", [Reason]),
+	    halt(1)
+    end.
 
-start(Dir, Port) ->
+start(Opts) ->
     application:start(ranch),
     application:start(crypto),
     application:start(cowlib),
     application:start(cowboy),
+    Dir = proplists:get_value(dir, Opts),
+    Port = proplists:get_value(port, Opts),
     Handler = {"/[...]", cowboy_static, 
 	       {dir, Dir, [{mimetypes, cow_mimetypes, all}]}},
     Dispatch = cowboy_router:compile([{'_', [Handler]}]),
+    CowboyOpts = case proplists:get_bool(verbose, Opts) of
+		     true ->
+			 [{onresponse, fun ?MODULE:onresponse/4}];
+		     false ->
+			 []
+		 end,
     case cowboy:start_http(http, 5, [{port, Port}], 
-			   [{env, [{dispatch, Dispatch}]}]) of
+			   [{env, [{dispatch, Dispatch}]} | CowboyOpts]) of
 	{ok, _} ->
-	    ?info("Serving ~p on 0.0.0.0:~p~n", [Dir, Port]),
+	    ?info("Serving ~p on 0.0.0.0:~p~n", [Dir, proplists:get_value(port, Opts)]),
 	    loop();
 	{error, Err} ->
 	    ?error("Error starting server: ~p~n", [Err]),
@@ -60,3 +100,8 @@ loop() ->
 	    loop()
     end.
 
+onresponse(Status, _Headers, _Body, Req) ->
+    {Method, _} = cowboy_req:method(Req),
+    {Path, _} = cowboy_req:path(Req),
+    ?info("~s ~s - ~p~n", [Method, Path, Status]),
+    Req.
